25844ea35773810f1a317d7469a00206d04ab00f
duc68456 <duc68456@gmail.com>
2025-09-25 11:03:38 +0700

    fix syntax error vite config

--- introdemo/vite.config.js
+++ introdemo/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      'api': 'http://localhost:3001'
-    }
+      '/api': 'http://localhost:3001'
+    },
+    port: 5173,
   }
 })
